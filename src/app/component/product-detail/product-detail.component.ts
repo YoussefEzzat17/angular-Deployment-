fb2a65cecb216c7c9e8c782be479037a89795cc6
Youssef Ezzat <ezzatyoussef79@gmail.com>
2025-05-21 17:40:56 +0300

    Add files via upload

---
+++ src/app/component/product-detail/product-detail.component.ts
@@ -0,0 +1,344 @@
+import { ProductService } from '../../util/services/product.service';
+import { faHeart, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { CommonModule } from '@angular/common';
+import { jwtDecode } from 'jwt-decode';
+import { DecodedToken } from '../../util/interfaces/iproduct';
+import { FormsModule } from '@angular/forms';
+import { CartService } from '../../util/services/cart.service';
+import { DashboardService } from '../../util/services/dashboard.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { WishlistService } from '../../util/services/wishlist.service';
+import { BehaviorSubject, map, switchMap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [
+    FontAwesomeModule,
+    CarouselModule,
+    RouterModule,
+    CommonModule,
+    FormsModule,
+  ],
+  standalone: true,
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css'],
+  providers: [ProductService, DashboardService],
+})
+export class ProductDetailComponent implements OnInit {
+  faHeart = faHeart;
+  faPaperPlane = faPaperPlane;
+  private wishlistService = inject(WishlistService);
+  private readonly loadData$ = new BehaviorSubject(true);
+  wishlistItems = toSignal(this.loadWhishList);
+  get loadWhishList() {
+    return this.loadData$.pipe(
+      switchMap(() =>
+        this.wishlistService.loadWishlist().pipe(map((res) => res.wishlist))
+      )
+    );
+  }
+
+  mainCarouselOptions = {
+    items: 1,
+    dots: true,
+    nav: false,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 4000,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+    },
+  };
+
+  productDetails: {
+    _id: string;
+    title: string;
+    category: { name: string };
+    images: string[];
+    price: number;
+    description: string;
+    quantity: number;
+  } = {
+    _id: '',
+    title: '',
+    category: { name: '' },
+    images: [],
+    price: 0,
+    description: '',
+    quantity: 0,
+  };
+
+  productComments: {
+    _id: string;
+    userId: string;
+    comment: string;
+    userName: string;
+    createdAt: Date;
+    avatar: string;
+  }[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private ProductService: ProductService,
+    private cdr: ChangeDetectorRef,
+    private dashboardService: DashboardService,
+    private router: Router,
+    private cartService: CartService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  user: {
+    userID?: string;
+    userName?: string;
+    avatar?: string;
+    role?: string;
+  } = {};
+
+  serverURL = 'http://localhost:4000/uploads/';
+
+  getAllComments() {
+    const productId = this.route.snapshot.params['id'];
+
+    this.ProductService.getAllComments(productId).subscribe({
+      next: (res) => {
+        this.productComments = res.data.Comments;
+        this.getAvatarForEveryUser();
+        this.cdr.detectChanges();
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  getProductDetails() {
+    const productId = this.route.snapshot.params['id'];
+
+    this.ProductService.getSpecificProduct(productId).subscribe({
+      next: (res) => {
+        this.productDetails = res.product[0];
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  isAdmin: boolean = false;
+
+  ngOnInit(): void {
+    const token = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('userToken='))
+      ?.split('=')[1];
+
+    this.user = jwtDecode<DecodedToken>(token as string);
+
+    if (this.user.role === 'admin') {
+      this.isAdmin = true;
+    } else {
+      this.isAdmin = false;
+    }
+
+    this.getProductDetails();
+    this.getAllComments();
+  }
+
+  userComment = '';
+
+  addComment() {
+    const productId = this.route.snapshot.params['id'];
+
+    const comment = {
+      userId: this.user.userID,
+      userName: this.user.userName,
+      comment: this.userComment,
+      createdAt: new Date(),
+    };
+
+    this.ProductService.addComment(productId, comment).subscribe({
+      next: (res) => {
+        console.log('comment', res);
+
+        this.productComments = [...res.data.Comments];
+        this.userComment = '';
+        this.getAvatarForEveryUser();
+        this.cdr.detectChanges();
+
+        this.snackBar.open(res.message, '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+      },
+      error: (err) => {
+        this.snackBar.open('Failed in added Comment', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+        console.error(err);
+      },
+    });
+  }
+
+  getAvatarForEveryUser() {
+    const newProductComments = this.productComments.map((comment) => {
+      this.dashboardService.getUserById(comment.userId).subscribe({
+        next: (res) => {
+          comment.avatar = res.data[0].avatar;
+          comment.userName = res.data[0].username;
+        },
+        error: (err) => console.error(err),
+      });
+    });
+  }
+
+  deleteComment(productId: string, commentId: string) {
+    this.ProductService.deleteComment(productId, commentId).subscribe({
+      next: (res) => {
+        this.productComments = res.data;
+        this.getAvatarForEveryUser();
+        this.snackBar.open(res.message, '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+      },
+      error: (err) => {
+        this.snackBar.open('Failed in delete comment', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+        console.error(err);
+      },
+    });
+  }
+
+  editPopupIsOpened = false;
+
+  commentIdForEdit = '';
+  editPopupIsOpen(commentId: string) {
+    this.commentIdForEdit = commentId;
+
+    const myComment = this.productComments.filter(
+      (comment) => comment._id === commentId
+    );
+
+    this.newComment = myComment[0]?.comment || '';
+    this.editPopupIsOpened = true;
+  }
+
+  editPopupIsClosed() {
+    this.editPopupIsOpened = false;
+  }
+
+  newComment = '';
+  editComment(productId: string) {
+    this.ProductService.editComment(
+      productId,
+      this.commentIdForEdit,
+      this.newComment
+    ).subscribe({
+      next: (res) => {
+        this.productComments = res.data;
+        this.editPopupIsOpened = false;
+        this.newComment = '';
+        this.getAvatarForEveryUser();
+
+        this.snackBar.open(res.message, '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+      },
+      error: (err) => {
+        this.snackBar.open('Failed in edit comment', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+        console.error(err);
+      },
+    });
+  }
+
+  addToCart(productId: string) {
+    this.cartService.addToCart(productId).subscribe(
+      (response) => {
+        if (response.status === 'success') {
+          this.snackBar.open('Product added to cart!', '', {
+            duration: 4000,
+            panelClass: ['custom-snackbar'],
+          });
+          this.router.navigate(['/cart']);
+        }
+      },
+      (error) => {
+        if (error.error.message === 'Product already in cart') {
+          this.snackBar.open('Product is already in your cart!', '', {
+            duration: 4000,
+            panelClass: ['custom-snackbar'],
+          });
+        } else {
+          this.snackBar.open('Something went wrong!', '', {
+            duration: 4000,
+            panelClass: ['custom-snackbar'],
+          });
+          console.error('Add to cart error:', error);
+        }
+      }
+    );
+  }
+  toggleWishlist(id: string): void {
+    const action = this.isItemInWhishlist(id)
+      ? this.removeFromWhishlist
+      : this.addToWhishlist;
+
+    action.call(this, id);
+  }
+
+  isItemInWhishlist(id: string) {
+    return this.wishlistItems()?.find((item) => item._id === id);
+  }
+
+  removeFromWhishlist(productId: string): void {
+    this.wishlistService.removeFromWishlist(productId).subscribe({
+      next: () => {
+        this.loadData$.next(true);
+        this.snackBar.open('Product removed from wishlist successfully', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+      },
+      error: (err) => {
+        this.snackBar.open('Failed in removed product from wishlist', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+        console.error('Error removing item:', err);
+      },
+    });
+  }
+  addToWhishlist(productId: string): void {
+    this.wishlistService.addToWishlist(productId).subscribe({
+      next: () => {
+        this.loadData$.next(true);
+        this.snackBar.open('Product added to wishlist successfully', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+      },
+      error: (err) => {
+        this.snackBar.open('Failed in added product to wishlist', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+        console.error('Error removing item:', err);
+      },
+    });
+  }
+}
---
+++ src/app/component/product/product.component.ts
@@ -0,0 +1,212 @@
+import { Component, inject, Input } from '@angular/core';
+import { ProductService } from '../../util/services/product.service';
+import { DecodedToken, Iproduct } from '../../util/interfaces/iproduct';
+import { CookieService } from 'ngx-cookie-service';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { WishlistService } from '../../util/services/wishlist.service';
+import { BehaviorSubject, map, switchMap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { jwtDecode } from 'jwt-decode';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CartService } from '../../util/services/cart.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [RouterModule, CommonModule, FormsModule, CarouselModule],
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css'],
+})
+export class ProductComponent {
+  @Input() productListLength: number = 0;
+  productList: Iproduct[] = [];
+  filteredProductList: Iproduct[] = [];
+  searchQuery: string = '';
+
+  carouselOptions = {
+    items: 1,
+    dots: true,
+    nav: false,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 4000,
+    margin: 10,
+  };
+  private wishlistService = inject(WishlistService);
+  private productService = inject(ProductService);
+  private cookieService = inject(CookieService);
+  private readonly loadData$ = new BehaviorSubject(true);
+  wishlistItems = toSignal(this.loadWhishList);
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private cartService: CartService,
+    private router: Router
+  ) {}
+
+  get loadWhishList() {
+    return this.loadData$.pipe(
+      switchMap(() =>
+        this.wishlistService.loadWishlist().pipe(map((res) => res.wishlist))
+      )
+    );
+  }
+
+  serverURL = 'http://localhost:4000/uploads/';
+  isAdmin: boolean = false;
+
+  token = document.cookie
+    .split('; ')
+    .find((row) => row.startsWith('userToken='))
+    ?.split('=')[1];
+
+  randomIndexesOfProducts: number[] = [];
+
+  getRandomIndexesOfProducts(productList: Iproduct[]) {
+    this.randomIndexesOfProducts = [];
+
+    let count;
+    if (productList.length > 12) {
+      if (location.pathname === '/home') {
+        count = 12;
+      } else {
+        count = productList.length;
+      }
+    } else {
+      count = productList.length;
+    }
+
+    while (this.randomIndexesOfProducts.length < count) {
+      const randomNum = Math.floor(Math.random() * productList.length);
+      if (!this.randomIndexesOfProducts.includes(randomNum)) {
+        this.randomIndexesOfProducts.push(randomNum);
+      }
+    }
+    console.log(this.randomIndexesOfProducts);
+  }
+
+  ngOnInit(): void {
+    const user = jwtDecode<DecodedToken>(this.token as string);
+    if (user.role === 'admin') {
+      this.isAdmin = true;
+    } else {
+      this.isAdmin = false;
+    }
+
+    this.productService.getAllProducts().subscribe({
+      next: (res) => {
+        this.productList = res.products.map((product: any) => ({
+          ...product,
+          ratingsAverage: this.getRandomRating(),
+        }));
+
+        this.filteredProductList = [...this.productList];
+        this.getRandomIndexesOfProducts(this.productList);
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  filterProducts(): void {
+    if (this.searchQuery.trim() === '') {
+      this.filteredProductList = [...this.productList];
+    } else {
+      this.filteredProductList = this.productList.filter((product) =>
+        product.title.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+    this.getRandomIndexesOfProducts(this.filteredProductList);
+  }
+
+  onSearchChange(): void {
+    this.filterProducts();
+  }
+
+  addToCart(productId: string) {
+    this.cartService.addToCart(productId).subscribe(
+      (response) => {
+        if (response.status === 'success') {
+          this.snackBar.open('Product added to cart!', '', {
+            duration: 3000,
+            panelClass: ['custom-snackbar'],
+          });
+          // this.router.navigate(['/cart']);
+        }
+      },
+      (error) => {
+        if (error.error.message === 'Product already in cart') {
+          this.snackBar.open('Product is already in your cart!', '', {
+            duration: 4000,
+            panelClass: ['custom-snackbar'],
+          });
+        } else {
+          this.snackBar.open('Something went wrong!', '', {
+            duration: 4000,
+            panelClass: ['custom-snackbar'],
+          });
+          console.error('Add to cart error:', error);
+        }
+      }
+    );
+  }
+
+  toggleWishlist(id: string): void {
+    const action = this.isItemInWhishlist(id)
+      ? this.removeFromWhishlist
+      : this.addToWhishlist;
+
+    action.call(this, id);
+  }
+
+  isItemInWhishlist(id: string) {
+    return this.wishlistItems()?.find((item) => item._id === id);
+  }
+
+  removeFromWhishlist(productId: string): void {
+    this.wishlistService.removeFromWishlist(productId).subscribe({
+      next: () => {
+        this.loadData$.next(true);
+        this.snackBar.open('Product removed from wishlist successfully', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+      },
+      error: (err) => {
+        this.snackBar.open('Failed in removed product from wishlist', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+        console.error('Error removing item:', err);
+      },
+    });
+  }
+  addToWhishlist(productId: string): void {
+    this.wishlistService.addToWishlist(productId).subscribe({
+      next: () => {
+        this.loadData$.next(true);
+        this.snackBar.open('Product added to wishlist successfully', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+      },
+      error: (err) => {
+        this.snackBar.open('Failed in added product to wishlist', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+        console.error('Error removing item:', err);
+      },
+    });
+  }
+
+  getRandomRating(): number {
+    const fullStars = Math.floor(Math.random() * 5) + 1;
+    const hasHalf = Math.random() < 0.5;
+    return hasHalf && fullStars < 5 ? fullStars + 0.5 : fullStars;
+  }
+}
