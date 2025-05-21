553b755e83a8bcb6b634e3ae255f406d4abf66e3
Youssef Ezzat <ezzatyoussef79@gmail.com>
2025-05-21 17:31:10 +0300

    Delete src directory

--- src/app/util/environment.ts
+++
@@ -1,7 +0,0 @@
-const environment={
-  baseUrl:"http://localhost:4000"
-}
-
-
-
-export {environment}
--- src/app/util/interfaces/feedback.ts
+++
@@ -1,6 +0,0 @@
-export interface IFeedback{
-  name:string,
-  email:string,
-  phone:string,
-  message:string
-}
