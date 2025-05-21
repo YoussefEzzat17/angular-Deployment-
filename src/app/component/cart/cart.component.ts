5d4b109d41bf264b99044eddcfe482d1ae926cd9
Youssef Ezzat <ezzatyoussef79@gmail.com>
2025-05-21 17:39:44 +0300

    Add files via upload

---
+++ src/app/component/cart/cart.component.ts
@@ -0,0 +1,170 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CartService } from '../../util/services/cart.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [CommonModule, RouterLink, FormsModule],
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css'],
+})
+export class CartComponent implements OnInit, OnDestroy {
+  products: any[] = [];
+  totalCheckout: number = 0;
+  couponCode: string = '';
+  couponApplied: boolean = false;
+  couponMessage: string = '';
+  discountAmount: number = 0;
+  isLoading: boolean = true;
+
+  cartCount: number = 0;
+  private subscriptions = new Subscription();
+
+  serverURL = 'http://localhost:4000/uploads/';
+
+  constructor(
+    private cartService: CartService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+    this.loadCart();
+
+    this.subscriptions.add(
+      this.cartService.cartCount$.subscribe((count) => {
+        this.cartCount = count;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  private updateLocalStorage() {
+    localStorage.setItem('productCart', JSON.stringify(this.products));
+  }
+
+  loadCart() {
+    this.isLoading = true;
+    this.cartService.getCart().subscribe(
+      (response) => {
+        if (response.status === 'success') {
+          this.products = response.cart.products;
+          this.calcCheckout();
+          this.updateLocalStorage();
+        }
+        this.isLoading = false;
+      },
+      (error) => {
+        console.error('Failed to load cart:', error);
+        this.isLoading = false;
+      }
+    );
+  }
+
+  increaseQuantity(productId: string) {
+    const item = this.products.find((p) => p.productId._id === productId);
+    if (item) {
+      item.quantity++;
+      this.updateQuantityInCart(productId, item.quantity);
+    }
+  }
+
+  decreaseQuantity(productId: string) {
+    const item = this.products.find((p) => p.productId._id === productId);
+    if (item && item.quantity > 1) {
+      item.quantity--;
+      this.updateQuantityInCart(productId, item.quantity);
+    }
+  }
+
+  updateQuantityInCart(productId: string, quantity: number) {
+    this.cartService.updateQuantity(productId, quantity).subscribe(
+      (response) => {
+        if (response.status === 'success') {
+          this.loadCart();
+        }
+      },
+      (error) => {
+        console.error('Failed to update quantity:', error);
+      }
+    );
+  }
+
+  deleteProduct(productId: string) {
+    this.cartService.removeProduct(productId).subscribe(
+      (response) => {
+        if (response.status === 'success') {
+          this.loadCart();
+          this.snackBar.open('Product deleted successfully', '', {
+            duration: 4000,
+            panelClass: ['custom-snackbar'],
+          });
+        }
+      },
+      (error) => {
+        this.snackBar.open('Failed in deleted Product', '', {
+          duration: 4000,
+          panelClass: ['custom-snackbar'],
+        });
+        console.error('Failed to remove product:', error);
+      }
+    );
+  }
+
+  calcCheckout() {
+    this.totalCheckout = this.products.reduce((acc, item) => {
+      const price = item.productId?.price || 0;
+      return acc + price * item.quantity;
+    }, 0);
+  }
+
+  clearCart() {
+    this.cartService.clearCart().subscribe(
+      (response) => {
+        if (response.status === 'success') {
+          this.products = [];
+          this.totalCheckout = 0;
+          this.updateLocalStorage();
+        }
+      },
+      (error) => {
+        console.error('Failed to clear cart:', error);
+      }
+    );
+  }
+
+  handelcoupon() {
+    if (!this.couponCode) {
+      this.couponMessage = 'Please enter a coupon code.';
+      this.couponApplied = false;
+      return;
+    }
+
+    this.cartService.applyCoupon(this.couponCode).subscribe(
+      (response) => {
+        if (response.status === 'success') {
+          this.totalCheckout = response.newTotal;
+          this.discountAmount = response.discount || 0;
+          this.couponApplied = true;
+          this.couponMessage =
+            response.message || 'Coupon applied successfully!';
+        } else {
+          this.couponApplied = false;
+          this.couponMessage = response.message || 'Invalid coupon.';
+        }
+      },
+      (error) => {
+        console.error('Failed to apply coupon:', error);
+        this.couponApplied = false;
+        this.couponMessage = 'Invalid coupon code. Please try again';
+      }
+    );
+  }
+}
---
+++ src/app/component/customers-dashboard/customers-dashboard.component.ts
@@ -0,0 +1,94 @@
+import { DashboardService } from './../../util/services/dashboard.service';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+import { DecodedToken } from '../../util/interfaces/iproduct';
+
+@Component({
+  selector: 'app-customers-dashboard',
+  imports: [],
+  templateUrl: './customers-dashboard.component.html',
+  styleUrl: './customers-dashboard.component.css',
+})
+export class CustomersDashboardComponent {
+  customersAllOrders = [
+    {
+      _id: '',
+      products: [{ title: '', adminId: '' }],
+      userDetails: {
+        _id: '',
+        username: '',
+        avatar: '',
+        phone: '',
+        email: '',
+      },
+      total: '',
+      createdAt: '',
+      userId: '',
+    },
+  ];
+
+  constructor(
+    private DashboardService: DashboardService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  serverURL = 'http://localhost:4000/uploads/';
+  adminID = '';
+
+  ngOnInit(): void {
+    const token = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('userToken='))
+      ?.split('=')[1];
+
+    if (!token) {
+      throw new Error('User token not found in cookies');
+    }
+
+    const decodedToken = jwtDecode<DecodedToken>(token);
+    this.adminID = decodedToken.userID;
+
+    this.DashboardService.getAllAdminCustomers(decodedToken.userID).subscribe({
+      next: (res) => {
+        console.log(res.customers);
+        this.customersAllOrders = res.customers;
+        this.getAllUserData();
+      },
+      error: (err) => console.error(err),
+    });
+  }
+
+  uniqueUserID: string[] = [];
+
+  customersDetails: {
+    avatar: string;
+    username: string;
+    phone: string;
+    email: string;
+  }[] = [];
+
+  getAllUserData(): void {
+    for (const customer of this.customersAllOrders) {
+      const userId = customer.userDetails._id;
+      if (userId !== undefined && !this.uniqueUserID.includes(userId)) {
+        this.uniqueUserID.push(userId);
+        this.customersDetails.push(customer.userDetails);
+      }
+    }
+
+    console.log(this.uniqueUserID);
+    console.log(this.customersDetails);
+
+    let count = 0;
+    for (let userID of this.uniqueUserID) {
+      this.DashboardService.getUserById(userID).subscribe({
+        next: (res) => {
+          console.log(res);
+
+          this.customersDetails[count++] = res.data[0];
+        },
+        error: (err) => console.error(err),
+      });
+    }
+  }
+}
