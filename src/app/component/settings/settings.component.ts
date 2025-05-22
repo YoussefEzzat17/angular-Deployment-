21f6740ed682305b0f05d29b8150387d2cf9938e
Youssef Ezzat <ezzatyoussef79@gmail.com>
2025-05-22 20:40:41 +0300

    Update settings.component.ts

--- src/app/component/settings/settings.component.ts
+++ src/app/component/settings/settings.component.ts
@@ -37,7 +37,7 @@ export class SettingsComponent {
     email: '',
   };
 
-  serverURL = 'http://localhost:4000/uploads/';
+  serverURL = 'https://ecommerceapi-production-8d5f.up.railway.app/uploads/';
 
   ngOnInit(): void {
     const token = document.cookie
