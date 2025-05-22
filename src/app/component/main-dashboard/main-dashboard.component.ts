f5ca55791d8200a134609fa1640ca14df8faf891
Youssef Ezzat <ezzatyoussef79@gmail.com>
2025-05-22 20:37:24 +0300

    Update main-dashboard.component.ts

--- src/app/component/main-dashboard/main-dashboard.component.ts
+++ src/app/component/main-dashboard/main-dashboard.component.ts
@@ -164,5 +164,5 @@ export class MainDashboardComponent {
     }
   }
 
-  serverURL = 'http://localhost:4000/uploads/';
+  serverURL = 'https://ecommerceapi-production-8d5f.up.railway.app/uploads/';
 }
